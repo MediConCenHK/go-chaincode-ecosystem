d2bc2e139b37583a8f9d1b6a7fa53ad4944a6a90
davidLiu <david-khala@hotmail.com>
2018-11-12 21:07:41 +0800

    In progress: add tokenHistory

--- main.go
+++ main.go
@@ -23,39 +23,52 @@ func (t GlobalChaincode) Init(stub shim.ChaincodeStubInterface) (response peer.R
 	return shim.Success(nil)
 }
 
-func (t GlobalChaincode) putToken(cid ClientIdentity, params []string) {
-	var tokenID = params[0]
-	var tokenData TokenData
-	FromJson([]byte(params[1]), &tokenData)
+func (t GlobalChaincode) putToken(cid ClientIdentity, tokenID string, tokenData TokenData) {
 	tokenData.Client = cid
 	t.PutStateObj(tokenID, tokenData)
 }
-func (t GlobalChaincode) getToken(cid ClientIdentity, params []string) []byte {
-	var tokenID = params[0]
+func (t GlobalChaincode) getToken(cid ClientIdentity, token string) []byte {
 	var tokenData TokenData
-	var exist = t.GetStateObj(tokenID, &tokenData)
+	var exist = t.GetStateObj(token, &tokenData)
 	if ! exist {
 		return nil
 	}
-	//TODO more logic here
 	return ToJson(tokenData)
 }
-func (t GlobalChaincode) transferToken(cid ClientIdentity, params []string) []byte {
-	var tokenID = params[0]
-	var tokenTransferRequest TokenTransferRequest
-	FromJson([]byte(params[1]), &tokenTransferRequest)
+func (t GlobalChaincode) transferToken(cid ClientIdentity, token string, request TokenTransferRequest) []byte {
+
 	var tokenData TokenData
-	var exist = t.GetStateObj(tokenID, &tokenData)
+	var exist = t.GetStateObj(token, &tokenData)
 	if ! exist {
-		PanicString("token " + tokenID + " not exist")
+		PanicString("token " + token + " not exist")
 	}
-	if tokenData.Owner != tokenTransferRequest.FromOwner || tokenData.OwnerType != tokenTransferRequest.FromOwnerType {
-		PanicString("token " + tokenID + " does not belong to [" + tokenTransferRequest.FromOwnerType.To() + "]" + tokenTransferRequest.FromOwner)
+	if tokenData.Owner != request.FromOwner || tokenData.OwnerType != request.FromOwnerType {
+		PanicString("token " + token + " does not belong to [" + request.FromOwnerType.To() + "]" + request.FromOwner)
 	}
-	tokenData.Owner = tokenTransferRequest.ToOwner
-	tokenData.OwnerType = tokenTransferRequest.ToOwnerType
-	t.PutStateObj(tokenID, tokenData)
+	tokenData.Owner = request.ToOwner
+	tokenData.OwnerType = request.ToOwnerType
+	tokenData.Client = cid
+	t.PutStateObj(token, tokenData)
 	return ToJson(tokenData)
+}
+func (t GlobalChaincode) history(token string) []byte {
+	var filter = func(modification interface{}) bool {
+		return true
+	}
+	var history = ParseHistory(t.GetHistoryForKey(token), filter)
+	return ToJson(history)
+
+}
+func panicToken(token string) {
+	if token == "" {
+		PanicString("token is empty")
+	}
+}
+func panicTokenData(tokenData string) []byte {
+	if tokenData == "" {
+		PanicString("tokenData is empty")
+	}
+	return []byte(tokenData)
 }
 func (t GlobalChaincode) Invoke(stub shim.ChaincodeStubInterface) (response peer.Response) {
 	defer Deferred(DeferHandlerPeerResponse, &response)
@@ -67,15 +80,30 @@ func (t GlobalChaincode) Invoke(stub shim.ChaincodeStubInterface) (response peer
 	var clientID = NewClientIdentity(stub)
 	var transient = t.GetTransient()
 	var responseBytes []byte
+	const Fcn_tokenHistory = "tokenHistory"
 	switch fcn {
 	case Fcn_putToken:
 		t.InsuranceAuth.Exec(transient)
-		t.putToken(clientID, params)
+		var tokenID = params[0] //TODO nil check
+		panicToken(tokenID)
+		var tokenData TokenData //TODO nil check
+		FromJson(panicTokenData(params[1]), &tokenData)
+		t.putToken(clientID, tokenID, tokenData)
 	case Fcn_getToken:
-		responseBytes = t.getToken(clientID, params)
+		var tokenID = params[0]
+		panicToken(tokenID)
+		responseBytes = t.getToken(clientID, tokenID)
 	case Fcn_transferToken:
 		t.InsuranceAuth.Exec(transient) //TODO modify case
-		responseBytes = t.transferToken(clientID, params)
+		var tokenID = params[0]
+		panicToken(tokenID)
+		var tokenTransferRequest TokenTransferRequest
+		FromJson(panicTokenData(params[1]), &tokenTransferRequest)
+		responseBytes = t.transferToken(clientID, tokenID, tokenTransferRequest)
+	case Fcn_tokenHistory:
+		var tokenID = params[0]
+		panicToken(tokenID)
+		responseBytes = t.history(tokenID)
 	default:
 		PanicString("unknown fcn:" + fcn)
 	}
